f57a21f439a9e65168963474aaed8390d7106292
jsalaiz <jonathan.salaiz@gmail.com>
2021-09-19 21:45:46 -0400

    Completed the exercise

---
+++ BasicClassesExercise/BasicClassesExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BasicClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Mustang";
+            myCar.Year = 2010;
+            Console.WriteLine($"My car is a {myCar.Year} {myCar.Make} {myCar.Model}");
+        }
+    }
+}
