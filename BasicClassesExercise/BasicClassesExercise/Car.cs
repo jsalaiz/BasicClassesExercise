fcb82c4adfd6c2d5afc3d9c0aee6ec5cc349a7f8
jsalaiz <jonathan.salaiz@gmail.com>
2021-09-20 20:56:38 -0400

    forgot get and set. added them now

--- BasicClassesExercise/BasicClassesExercise/Car.cs
+++ BasicClassesExercise/BasicClassesExercise/Car.cs
@@ -8,8 +8,8 @@ namespace BasicClassesExercise
 {
     public class Car
     {
-        public string Make;
-        public string Model;
-        public int Year;
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
     }
 }
